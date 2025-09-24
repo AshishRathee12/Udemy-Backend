00da404d5733d9d90ceff754e5e82a294fe787f2
Aashish-Rathi <ashishrathi0002@gmail.com>
2025-09-24 19:40:06 +0530

    implement validation

--- Frontend/src/places/pages/NewPlace.jsx
+++ Frontend/src/places/pages/NewPlace.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import './NewPlace.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useFormik } from "formik";
+import { signUpSchema } from '../../shared/components/FormElements/Schemas';
+const initialValues = {
+  name: '',
+  email: ''
+}
 
 export default function NewPlace() {
+  // all are provide by formik we have to just write in code 
+  // const formik = useFormik({
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues,
+    validationSchema: signUpSchema,
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      // setSubmitting(false);
+      console.log(values); // stops default submission
+      resetForm(); //for clearing the data after submission
+
+    }
+  });
+  // console.log(formik)
+
   return (
-    <div>
-      added new places
-    </div>
-  )
+    <form className='form-container' onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          autoComplete='off'
+          name="name"
+          id="name"
+          placeholder="Name"
+          // provided by default by formik both values and onchange
+          value={values.name}
+          onChange={handleChange} //these are returned by formik automatically
+          onBlur={handleBlur}
+        />
+        {errors.name && touched.name ? <p className='form-error'>{errors.name}</p> : null}
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          autoComplete='off'
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.email && touched.email ? <p className='form-error'>{errors.email}</p> : null}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
---
+++ Frontend/src/shared/components/FormElements/Schemas.jsx
@@ -0,0 +1,6 @@
+import * as Yup from "yup"
+
+export const signUpSchema = Yup.object({
+    name: Yup.string("Must be of string type").min(2, "Must be more then 2 letter").max(25, "Must be less then 25 letters").required("Please Enter Your Name"),
+    email: Yup.string("Must be of String type").email("Invalid email format").required("Please enter your mail"),
+})
